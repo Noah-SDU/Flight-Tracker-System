464b079f21ae147799d0c1caddbcc8b28d1087a7
Noah-SDU <nogau25@student.sdu.dk>
2026-04-21 17:13:24 +0200

    Analytics view

---
+++ FlightTrackerSystem/Views/AnalyticsView.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FlightTrackerSystem.Views;
+
+public partial class AnalyticsView : UserControl
+{
+    public AnalyticsView()
+    {
+        InitializeComponent();
+    }
+}
